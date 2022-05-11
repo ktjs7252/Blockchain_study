c4c6485f7fc5648ecd4284fade26b99bde3449e1
ktjs7252 <78638160+ktjs7252@users.noreply.github.com>
2022-05-11 14:29:23 +0900

    Update UniswapV2Factory.sol

--- UniswapV2Factory.sol
+++ UniswapV2Factory.sol
@@ -20,14 +20,14 @@ contract UniswapV2Factory is IUniswapV2Factory {
         return allPairs.length;
     }
 
-    function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
-        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
+    function createPair(address tokenA, address tokenB) external returns (address pair) { //
+        require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');         // 토큰A,B가 같지 않은지 체크
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);  // 토큰 두개를 정렬
+        require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');  //주솟값은 0으로 시작될수 없다. 체크
+        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient   //소유자가 같은 토큰이 이미 가지고 있는지 체크
         bytes memory bytecode = type(UniswapV2Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-        assembly {
+        assembly { //어셈블리언어로 {}가 있으면 그 안에서만 사용할 수 있다;.
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
         IUniswapV2Pair(pair).initialize(token0, token1);
