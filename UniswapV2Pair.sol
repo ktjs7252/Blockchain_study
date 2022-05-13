9224045b09821d9639f5da190a8c12d7eab2bdbb
ktjs7252 <78638160+ktjs7252@users.noreply.github.com>
2022-05-14 04:25:30 +0900

    Update UniswapV2Pair.sol

--- UniswapV2Pair.sol
+++ UniswapV2Pair.sol
@@ -23,8 +23,8 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     uint112 private reserve1;           // uses single storage slot, accessible via getReserves
     uint32  private blockTimestampLast; // uses single storage slot, accessible via getReserves
 
-    uint public price0CumulativeLast;
-    uint public price1CumulativeLast;
+    uint public price0CumulativeLast;      //다른 함수에서 oracle price보려면 이거가져다 쓰면됨
+    uint public price1CumulativeLast;      // 그럼 시간에 따른 가격을 볼 수 있다. 
     uint public kLast; // reserve0 * reserve1, as of immediately after the most recent liquidity event
 
     uint private unlocked = 1;
@@ -58,24 +58,25 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     );
     event Sync(uint112 reserve0, uint112 reserve1);
 
-    constructor() public {
-        factory = msg.sender;
+    constructor() public {          //팩토리주소를 넣어줌
+        factory = msg.sender;       //트랜잭션을 보낸 address를 나타냄
     }
 
     // called once by the factory at time of deployment
     function initialize(address _token0, address _token1) external {
         require(msg.sender == factory, 'UniswapV2: FORBIDDEN'); // sufficient check
-        token0 = _token0;
-        token1 = _token1;
+        token0 = _token0;  //대입
+        token1 = _token1;  //대입
     }
 
     // update reserves and, on the first call per block, price accumulators
-    function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
+    function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {  //자신이 계속해서 얼마를 들고있는지를 추적 
+        // 항상 맞지를 않음 왜냐하면 실제 잔고는 토큰 컨트랙트에 있고 내가 지금 보고 있는 토큰의 수량이 달라지면 이득이 생겼음 - >입금을 알 수 잇다.
         require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'UniswapV2: OVERFLOW');
-        uint32 blockTimestamp = uint32(block.timestamp % 2**32);
+        uint32 blockTimestamp = uint32(block.timestamp % 2**32);                                
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
-        if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
-            // * never overflows, and + overflow is desired
+        if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {                 // 유니스왑을 price oracle로 쓰기 위한 로직
+            // * never overflows, and + overflow is desired               //각각의 price들이 유지된 시간의 가중치만큼 더해서 평균을 내서 현재의 가격을 결정
             price0CumulativeLast += uint(UQ112x112.encode(_reserve1).uqdiv(_reserve0)) * timeElapsed;
             price1CumulativeLast += uint(UQ112x112.encode(_reserve0).uqdiv(_reserve1)) * timeElapsed;
         }
@@ -86,7 +87,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     }
 
     // if fee is on, mint liquidity equivalent to 1/6th of the growth in sqrt(k)
-    function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
+    function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) { //수수료 계산하는 공식
         address feeTo = IUniswapV2Factory(factory).feeTo();
         feeOn = feeTo != address(0);
         uint _kLast = kLast; // gas savings
@@ -107,27 +108,27 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     }
 
     // this low-level function should be called from a contract which performs important safety checks
-    function mint(address to) external lock returns (uint liquidity) {
+    function mint(address to) external lock returns (uint liquidity) {     //LP토큰 만들어내는 함수 , 수량에 따른 민트인지 번인지
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
-        uint balance0 = IERC20(token0).balanceOf(address(this));
-        uint balance1 = IERC20(token1).balanceOf(address(this));
-        uint amount0 = balance0.sub(_reserve0);
-        uint amount1 = balance1.sub(_reserve1);
+        uint balance0 = IERC20(token0).balanceOf(address(this));    //토큰 0잔고 받아옴
+        uint balance1 = IERC20(token1).balanceOf(address(this));    //토큰 1잔고 받아옴
+        uint amount0 = balance0.sub(_reserve0);   //현재의 잔고  -> 두개를 빼면 알수 있음
+        uint amount1 = balance1.sub(_reserve1);   //  
 
-        bool feeOn = _mintFee(_reserve0, _reserve1);
+        bool feeOn = _mintFee(_reserve0, _reserve1); //인플레이션 수수료계산 먼저
         uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
-        if (_totalSupply == 0) {
-            liquidity = Math.sqrt(amount0.mul(amount1)).sub(MINIMUM_LIQUIDITY);
-           _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
+        if (_totalSupply == 0) {  // totlasupply가 0이면 처음pair 를 넣는 사람 / lp share의 최소단위, 즉1 LP토큰의 천배의 해당하는 돈이 burn
+            liquidity = Math.sqrt(amount0.mul(amount1)).sub(MINIMUM_LIQUIDITY); // 기하평군값에서 뒤에껄 뺌
+           _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens //세상에 존재하지 않으므로 묶인돈임 tmp처럼 쓰인다는 뜻
         } else {
-            liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
+            liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1); //두개중 더적은걸로 lp토큰 생성
         }
         require(liquidity > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
-        _mint(to, liquidity);
+        _mint(to, liquidity);  // lp토큰 생성
 
-        _update(balance0, balance1, _reserve0, _reserve1);
-        if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
-        emit Mint(msg.sender, amount0, amount1);
+        _update(balance0, balance1, _reserve0, _reserve1);  //업데이트
+        if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date //업데이트
+        emit Mint(msg.sender, amount0, amount1); //로그를 남김
     }
 
     // this low-level function should be called from a contract which performs important safety checks
@@ -156,7 +157,10 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     }
 
     // this low-level function should be called from a contract which performs important safety checks
-    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock { //플래시론 : 유니스왑에 꼭 한 토큰만 집어 넣지 않는 경우가 있다
+        //암호 화폐 대출 서비스 : 토큰을 먼저 빌리고 스왑하고 그걸로 다른데에 넣고 차액을 얻은 다음에 다시 반납하는 : 바이낸스의 마진거래랑 비슷한가? 그경우를 대비
+        //in은 스왑함수를 수행시키전에 토큰 컨트랙트에서 직접 보내버림 -> 백서상 이렇게 되면 in에 대한 변수가 없으므로 돈을 얼마나 보냈는지 알수없다 즉 프라이버시 지킬수있다??
+        //근데 토큰량으로 그냥 알 수 있는 거 아닌가?
         require(amount0Out > 0 || amount1Out > 0, 'UniswapV2: INSUFFICIENT_OUTPUT_AMOUNT');
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
         require(amount0Out < _reserve0 && amount1Out < _reserve1, 'UniswapV2: INSUFFICIENT_LIQUIDITY');
@@ -167,27 +171,27 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         address _token0 = token0;
         address _token1 = token1;
         require(to != _token0 && to != _token1, 'UniswapV2: INVALID_TO');
-        if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
+        if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens  //데이터를 ㅇ보냄
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-        if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
-        balance0 = IERC20(_token0).balanceOf(address(this));
+        if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data); //데이터가 있다면 호출 -> 이게 대출을 상환하는 코드?
+        balance0 = IERC20(_token0).balanceOf(address(this)); //입금했는지 확인하기 위해
         balance1 = IERC20(_token1).balanceOf(address(this));
         }
-        uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
+        uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0; //잘못됬으면 0으로 해주네
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
-        require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
+        require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT'); //0보다 커야지 통과됌 아니면 입금금액이 부족하다고 뜸->이게 약간 그 출금할때 못빼는 금액이랑 비슷
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
-        uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
+        uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3)); //또 수수료 내라고 하네
         uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
-        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'UniswapV2: K');
+        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'UniswapV2: K');  //확인
         }
 
-        _update(balance0, balance1, _reserve0, _reserve1);
-        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+        _update(balance0, balance1, _reserve0, _reserve1);  //업데이트
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to); //로그
     }
 
     // force balances to match reserves
-    function skim(address to) external lock {
+    function skim(address to) external lock {  //pair 컨트랙트에 담길 수 있는 토큰의 양을 넘었다면 남는 여유분을 회수-> 안하면 k값 고정이라 로직망가짐
         address _token0 = token0; // gas savings
         address _token1 = token1; // gas savings
         _safeTransfer(_token0, to, IERC20(_token0).balanceOf(address(this)).sub(reserve0));
@@ -195,7 +199,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     }
 
     // force reserves to match balances
-    function sync() external lock {
+    function sync() external lock { //반대로 돈이 적은데 inbalance 생겼을 때 다시 맞춰줌
         _update(IERC20(token0).balanceOf(address(this)), IERC20(token1).balanceOf(address(this)), reserve0, reserve1);
     }
 }
